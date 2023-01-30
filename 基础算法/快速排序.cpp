7067cde01ce18ca2a0ef7cd08eca4d43f73bacf9
sun <xin-sun@foxmail.com>
2023-01-30 15:12:06 +0800

    新增快速排序

---
+++ 基础算法/快速排序.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std; 
+
+const int N = 1e6 + 10;
+
+int n;
+int arr[N]; 
+
+// 快排
+void quick_sort(int arr[], int l, int r) {
+	// 递归边界
+	if(l >= r)
+		return;
+	
+	int i = l - 1, j = r + 1, x = arr[(l + r) >> 1];
+		
+	while(i < j) {
+		
+		// 左边始终大于或等于 x 
+		do i ++; while(x > arr[i]);
+		
+		// 右边始终小于或等于 x 
+		do j --; while(x < arr[j]);
+		
+		// 交换  
+		if(i < j) swap(arr[i], arr[j]); 
+	} 
+	
+	// 递归左右两边
+	quick_sort(arr, l, j);
+	quick_sort(arr, j+1, r); 
+} 
+
+int main() {
+	scanf("%d", &n);
+	
+	for(int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+		
+	// 快排
+	quick_sort(arr, 0 , n-1);
+	 
+	for(int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
